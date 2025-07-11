ec9a047b557ebde9c12b8a910ef4d0aab6201911
matanb123 <matanb1238@gmail.com>
2025-07-11 16:31:12 +0300

    Added svm notebook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('text.csv')  # or whatever the file name is
+df = pd.read_csv('data/text.csv')  # or whatever the file name is
 print(df.shape)               # outputs (number_of_rows, number_of_columns)
 print(df['label'].value_counts())  # optional: see distribution across emotion labels
